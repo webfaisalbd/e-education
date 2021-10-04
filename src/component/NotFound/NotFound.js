c6f04ea6f3e9e51b4d1eeb05f558ed97f44e61de
faisalcsebd <faisal.cse.bd@gmail.com>
2021-10-04 23:07:41 +0600

    ready for submit

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <p>I am not found mySelf</p>
+        <div className="heightStyle">
+            <h2 className="mt-4 w-50 mx-auto">Your url is not correct. Please recheck your url.
+            </h2>
         </div>
     );
 };
